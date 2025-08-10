74c58129af6bd3d633906ab1db2b8302eb44e5d0
kubow <kube.kubow@gmail.com>
2025-08-10 23:28:25 +0200

    regenerate docs with static links

---
+++ docs/_app/immutable/chunks/DgeQDQrb.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_at643?.base??"/vajdovi-web-presentation",a=globalThis.__sveltekit_at643?.assets??s;export{a,s as b};
--- docs/_app/immutable/entry/start.DZZ4-FjF.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/B6N9CAHG.js";export{o as load_css,r as start};
